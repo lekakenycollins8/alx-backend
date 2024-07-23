1663b28a7c4e238c1dd4d4baca74f94224da75c0
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-23 11:58:51 +0300

    returned a dict of dataset metadata details

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -45,3 +45,16 @@ class Server:
             return self.dataset()[start_index:end_index]
         except IndexError:
             return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """Get hypermedia pagination"""
+        data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+            'page_size': len(data),
+            'page': page,
+            'data': data,
+            'next_page': page + 1 if page + 1 < total_pages else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total_pages
+        }
