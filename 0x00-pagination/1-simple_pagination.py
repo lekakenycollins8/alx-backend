dc5b047073af56a1c9dcb9dc98604a4a5be2cd3c
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-23 11:49:15 +0300

    used baby names dataset and paginated the data in the csv file

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Simple helper function to paginate range of items"""
+
+
+import csv
+import math
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """Return a tuple of size two containing a start index and an end index"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Get page from dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        
+        try:
+            index_range_t = index_range(page, page_size)
+            start_index = index_range_t[0]
+            end_index = index_range_t[1]
+        
+            return self.dataset()[start_index:end_index]
+        except IndexError:
+            return []
