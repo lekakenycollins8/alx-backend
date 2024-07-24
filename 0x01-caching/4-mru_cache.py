31732e825f06a4938acaea319345523e49f446b3
Collins Lekakeny <lekakenycollins8@gmail.com>
2024-07-24 18:46:00 +0300

    Most recently used caching policy

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Most Recently Used caching replacement policy"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """MRU caching replacement policy"""
+    def __init__(self):
+        super().__init__()
+        self.stack = []
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.stack.remove(key)
+            self.stack.append(key)
+            return
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            removed = self.stack.pop()
+            del self.cache_data[removed]
+            print("DISCARD: {}".format(removed))
+        self.cache_data[key] = item
+        self.stack.append(key)
+
+    def get(self, key):
+        """Get an item by key"""
+        if key in self.cache_data:
+            self.stack.remove(key)
+            self.stack.append(key)
+        return self.cache_data.get(key, None)
